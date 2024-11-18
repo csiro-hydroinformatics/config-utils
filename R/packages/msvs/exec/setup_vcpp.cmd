44088388bfd7909c9779f13c73967c39512ff427
Jean-Michel Perraud <per202@csiro.au>
2024-11-18 17:03:38 +1100

    more error messages in setup_vcpp.cmd to try to
    inform and fail early. An error is not trapped in a pipeline

--- R/packages/msvs/exec/setup_vcpp.cmd
+++ R/packages/msvs/exec/setup_vcpp.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 @set exit_code=0
-
+@set error_msg=""
 
 REM load Visual Studio 2017 developer command prompt setup has changed compared to previous versions. 
 REM Inspired from: https://github.com/ctaggart  via https://github.com/Microsoft/visualfsharp/pull/2690/commits/bf52776167fe6a9f2354ea96094a025191dbd3e7
@@ -11,27 +11,31 @@ set VsDevCmdFile=\Common7\Tools\VsDevCmd.bat
 set progf=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\
 if exist "%progf%" (
     goto foundVsDevCmdFile
+) else (
+    echo "WARNING: failed to locate VS2019 at %progf%"
+    echo "WARNING: will try fallback options"
 )
 
+@REM # fallback 
 set progf=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\
+if exist "%progf%" (
+    goto foundVsDevCmdFile
+) else (
+    echo "WARNING: fallback to use VS2017 failed, no path found at %progf%"
+    echo "WARNING: will try 'build tool' fallback options"
+    goto fallbackVSCOMNTOOLS
+)
 
 :foundVsDevCmdFile
 
-if exist "%progf%Community%VsDevCmdFile%" (
-    call "%progf%Community%VsDevCmdFile%"
-    goto end
-)
-if exist "%progf%Professional%VsDevCmdFile%" (
-    call "%progf%Professional%VsDevCmdFile%"
-    goto end
-)
-if exist "%progf%Enterprise%VsDevCmdFile%" (
-    call "%progf%Enterprise%VsDevCmdFile%"
-    goto end
-)
-if exist "%progf%BuildTools%VsDevCmdFile%" (
-    call "%progf%BuildTools%VsDevCmdFile%"
-    goto end
+set "editions=Community Professional Enterprise BuildTools"
+
+for %%e in (%editions%) do (
+    if exist "%progf%%%e%VsDevCmdFile%" (
+        echo INFO: found "%progf%%%e%VsDevCmdFile%"
+        call "%progf%%%e%VsDevCmdFile%"
+        goto end
+    )
 )
 
 :fallbackVSCOMNTOOLS
@@ -42,9 +46,9 @@ if defined VSCOMNTOOLS (
 
 if defined VS140COMNTOOLS set VSCOMNTOOLS=%VS140COMNTOOLS%
 if defined VS140COMNTOOLS goto found
-if defined VS120COMNTOOLS set VSCOMNTOOLS=%VS140COMNTOOLS%
+if defined VS120COMNTOOLS set VSCOMNTOOLS=%VS120COMNTOOLS%
 if defined VS120COMNTOOLS goto found
-if defined VS110COMNTOOLS set VSCOMNTOOLS=%VS140COMNTOOLS%
+if defined VS110COMNTOOLS set VSCOMNTOOLS=%VS110COMNTOOLS%
 if defined VS110COMNTOOLS goto found
 
 @echo ERROR: Could not locate command prompt devenv setup for anything between VS2012 and VS2017
@@ -52,14 +56,10 @@ if defined VS110COMNTOOLS goto found
 @goto end
 
 :found
+echo "INFO: found env var for common tools under %VSCOMNTOOLS%"
 set VSDEVENV=%VSCOMNTOOLS%..\..\VC\vcvarsall.bat
 @if not exist "%VSDEVENV%" goto error_no_vcvarsall
 @call "%VSDEVENV%"
-@goto end
-
-:error_no_VS110COMNTOOLSDIR
-@echo ERROR: setup_vcpp cannot determine the location of the VS Common Tools folder.
-@set exit_code=1
 @goto end
 
 :error_no_vcvarsall
@@ -68,5 +68,10 @@ set VSDEVENV=%VSCOMNTOOLS%..\..\VC\vcvarsall.bat
 @goto end
 
 :end
+if %exit_code% neq 0 (
+    echo ERROR: %error_msg%
+) else (
+    echo INFO: setup_vcpp completed without error
+)
 exit /b %exit_code%
 
